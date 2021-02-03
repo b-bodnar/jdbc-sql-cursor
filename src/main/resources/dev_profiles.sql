d5034e90a4b2f73da5422915f362d968b0149120
Bohdan Bodnar <iloveyoulviv@gmail.com>
2021-02-03 14:53:33 +0200

    Changed task 3

--- src/main/resources/dev_profiles.sql
+++ src/main/resources/dev_profiles.sql
@@ -2,64 +2,68 @@ create database dev_profiles_db;
 
 create table accounts
 (
-	id int auto_increment primary key,
-	first_name varchar(45) not null,
-	last_name varchar(45) not null,
-	city varchar(45) not null,
-	gender varchar(10) not null,
-	username varchar(90) not null unique,
+    id         int auto_increment primary key,
+    first_name varchar(45) not null,
+    last_name  varchar(45) not null,
+    city       varchar(45) not null,
+    gender     varchar(10) not null,
+    username   varchar(90) not null unique
 );
 
 create table profiles_table
 (
-    id int auto_increment primary key,
-    username varchar(90)  not null unique,
-    job_title varchar(45)  not null,
-    department varchar(45)  not null,
-    company varchar(45)  not null,
-    skill text not null
+    id         int auto_increment primary key,
+    username   varchar(90) not null unique,
+    job_title  varchar(45) not null,
+    department varchar(45) not null,
+    company    varchar(45) not null,
+    skill      text        not null
 );
 
-rename table  profiles_table to profiles;
+rename table profiles_table to profiles;
 
-    -- 1 task
-select * from profiles
-where department='Sales'
+-- 1 task
+select *
+from profiles
+where department = 'Sales'
 order by skill;
 
-    -- 2 task
-select job_title from profiles
+-- 2 task
+select job_title
+from profiles
 group by job_title
 having count(*) > 3;
 
-    -- 3 task
+-- 3 task
 select accounts.first_name,
        accounts.last_name,
        profiles.job_title,
        profiles.company
 from accounts
          left join profiles
-                   on accounts.id = profiles.id;
+                   on accounts.id = profiles.id
+where city = 'Raglan';
 
-    -- 4 task
+-- 4 task
 select department, count(username) as employees
 from profiles
 group by department
 order by employees DESC;
 
-    -- 5 task
+-- 5 task
 select profiles.job_title, accounts.city
 from profiles
          left join accounts
                    on profiles.id = accounts.id
-where job_title = 'Editor'
+where job_title = 'Editor';
 
-    -- 6 task
-select * from accounts
+-- 6 task
+select *
+from accounts
 order by city;
 
 
-    -- 7 task
+-- 7 task
 update profiles
 set job_title = REPLACE(job_title, 'Engineer', 'Developer')
 where job_title like '%Engineer%';
